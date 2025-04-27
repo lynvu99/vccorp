71652ce425abbb4a8ecc673fe643f86b17e8b606
lynvu99 <thangvd1@runsystem.net>
2025-04-27 22:24:39 +0700

    some more css

--- vccorp/src/App.jsx
+++ vccorp/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       {/* Desktop/PC Version */}
       <div className="h-full relative text-white rounded-lg overflow-hidden shadow-lg">
         <div className=" desktop flex">
-          <div className="description h-120 lg:h-120  md:h-120 sm:h-100 w-full md:w-1/2 lg:w-2/5 flex flex-col justify-center absolute inset-y-0 right-0 md:right-8 lg:right-40 xl:right-100 md:right-8 sm:right-0 lg:top-48 md:top-52 sm:top-64 z-10">
+          <div className="description h-120 lg:h-120  md:h-120 sm:h-100 w-full md:w-1/2 lg:w-2/5 flex flex-col justify-center absolute inset-y-0 right-0 md:right-8 lg:right-40 xl:right-80 md:right-8 sm:right-0 lg:top-48 md:top-52 sm:top-64 z-10">
             <p className="fs-28">
               Tôi không nghĩ mình đang khác đi, bởi tôi là một người sống khá
               đơn giản. Tôi biết những người yêu mến tôi là vì sự chân thật và
